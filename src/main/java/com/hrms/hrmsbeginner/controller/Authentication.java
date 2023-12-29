8fae3b39cc29038ea68f8530d6e4355d1cf919e5
Arefin <ishraque.arefinrafi@gmail.com>
2023-12-30 00:56:33 +0600

    feat(controller/authentication)-first controller created

---
+++ src/main/java/com/hrms/hrmsbeginner/controller/Authentication.java
@@ -0,0 +1,14 @@
+package com.hrms.hrmsbeginner.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/authenticate")
+public class Authentication {
+@GetMapping
+    public String hello(){
+    return "hi";
+     }
+}
