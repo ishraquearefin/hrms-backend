9a761a305074c9c9bf43015821e116eac71c3a43
Arefin <ishraque.arefinrafi@gmail.com>
2023-12-30 00:03:36 +0600

    feat(file-added):initial commit

---
+++ src/main/java/com/hrms/hrmsbeginner/HrmsBeginnerApplication.java
@@ -0,0 +1,13 @@
+package com.hrms.hrmsbeginner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmsBeginnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrmsBeginnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hrms/hrmsbeginner/HrmsBeginnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrms.hrmsbeginner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrmsBeginnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
